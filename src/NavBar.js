4479346632e8d8c17da039eb2ce4bcd88a5ef951
Anna Neander <neander@kth.se>
2022-02-24 21:39:04 +0100

    navbar

---
+++ src/NavBar.js
@@ -0,0 +1,28 @@
+import React, {useEffect, useLayoutEffect} from "react";
+import * as d3 from "d3";
+
+
+/* just some test to see that d3 works*/
+function NavBar() {
+
+ let stat = 1;
+
+ /*  use LayoutEffect() for updating d3 components? */
+  useLayoutEffect(() => {
+    d3.select(".nav").style("background-color", "magenta");
+  }, []);
+
+  const onMouseClick = () => {
+    d3.selectAll(".nav").style("color", "yellow");
+  };
+
+
+  return (
+    /* use svg element */
+      <div className = "nav">
+        <h1>About</h1>
+      </div>
+  );
+}
+
+export default NavBar;
