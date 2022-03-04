469c06f809c25292d186e2ee33b934720cef1091
larissaw <larissaw@kth.se>
2022-03-04 12:09:48 +0100

    migration data

--- src/DataModel.js
+++ src/DataModel.js
@@ -1,38 +1,59 @@
-
 /* handles state (year, country) of the app and fetches data from database  */
 class DataModel {
-
-  constructor(year = '1980_1985', country = null) {
-  this.year = year
-  this.country = country;
-
-  //this.data = null;
-}
-
-  setYear(x){
-    this.year = x
+  constructor(year = "1980_1985", country = null) {
+    this.year = year;
+    this.country = country;
+    this.getMigrationData().then((res) => {
+      this.migrationData = res;
+      //this.data = null;
+    });
   }
-  setCountry(x){
-    this.country = x
+  getMigrationValue(origin, destination, year) {
+    if (this.migrationData)
+      return this.migrationData.filter(function findValue(data) {
+        return data.DestinationID == destination && data.OriginID == origin;
+      })[0][year];
+  }
+  setYear(x) {
+    this.year = x;
+  }
+  setCountry(x) {
+    this.country = x;
   }
 
   /* param: country
     return: number of incoming migrants per country as fields
     country: number
     TODO: error handling */
-  getData(x = ""){
+  getData(x = "") {
     /* fetch data for country x */
-    return fetch(`./data/${this.year}.json`
-      ,{headers : {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
-     }
-   })
-    .then(res => res.json())
-    .then(resData => {
-      return resData;
+    return fetch(`./data/${this.year}.json`, {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
     })
-    .catch(_ => console.log(_))
+      .then((res) => res.json())
+      .then((resData) => {
+        return resData;
+      })
+      .catch((_) => console.log(_));
+  }
+
+  getMigrationData() {
+    /* fetch data for country x */
+    return fetch(`./data/Data.json`, {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((resData) => {
+        console.log(resData);
+        return resData;
+      })
+      .catch((_) => console.log(_));
   }
 }
 export default DataModel;
